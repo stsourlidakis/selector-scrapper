a79ee19b4fd751c4f1ab0d0950bbc7be7b891ccb
stsourlidakis <s.tsourlidakis@hotmail.com>
2019-07-28 18:16:56 +0300

    Update script to work with the latest version of jsdom

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const jsdom = require("jsdom");
-const delay = 5;	//seconds
-let url = '';
-let ccsSelector = '';
+const { JSDOM } = require("jsdom");
+const delay = 5; //seconds
+let url = "";
+let ccsSelector = "";
 
 let previousValue;
 
-if( process.argv.length==4 ){
-	url = process.argv[2];
-	ccsSelector = process.argv[3];
+if (process.argv.length == 4) {
+  url = process.argv[2];
+  ccsSelector = process.argv[3];
 }
 
 checkValue();
-setInterval(checkValue, delay*1000);
+setInterval(checkValue, delay * 1000);
 
-function checkValue(){
-	jsdom.env(
-		url,
-		function (err, window) {
-			const newValue = window.document.querySelector(ccsSelector).textContent;
-			if( previousValue!==newValue && previousValue){
-				console.log(`Value changed from ${previousValue} to ${newValue}`);
-			} else {
-				console.log(`The value is ${newValue}`);
-			}
+function checkValue() {
+  JSDOM.fromURL(url).then(dom => {
+    const newValue = dom.window.document.querySelector(ccsSelector).textContent;
+    if (previousValue !== newValue && previousValue) {
+      console.log(`Value changed from ${previousValue} to ${newValue}`);
+    } else {
+      console.log(`The value is ${newValue}`);
+    }
 
-			previousValue = newValue;
-		}
-	);
+    previousValue = newValue;
+  });
 }
